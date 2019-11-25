011d7a28a191c52d28d85383fe4ee27537a889a1
vincentpoc <32473645+vincentpoc@users.noreply.github.com>
2019-11-25 13:07:54 -0500

    Init
    Scene 
    Event
    Group

---
+++ main.lua
@@ -0,0 +1,14 @@
+--------------------- INIT --------------------------------------------------------------------
+local composer = require ("composer")
+
+-- Hide status bar
+display.setStatusBar (display.HiddenStatusBar)
+
+-- Seed the random number generator
+math.randomseed (os.time () )
+
+-- Go to the menu screen
+composer.gotoScene ("game")
+
+-----------------------------------------------------------------------------------------
+
