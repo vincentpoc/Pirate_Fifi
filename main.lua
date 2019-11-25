39c5b651057706dc3f7fd573d8739db47c49dcc1
vincentpoc <32473645+vincentpoc@users.noreply.github.com>
2019-11-25 13:10:59 -0500

    Add files via upload
    scene options example

--- main.lua
+++ main.lua
@@ -7,8 +7,17 @@ display.setStatusBar (display.HiddenStatusBar)
 -- Seed the random number generator
 math.randomseed (os.time () )
 
--- Go to the menu screen
-composer.gotoScene ("game")
+-- Go to
+local options =
+{
+	effect = "fade",
+	time = 400,
+	params = {
+		sampleVar1 = "my sample variable",
+		sampleVar2 = "another sample variable"
+	}
+}
+composer.gotoScene ("game", options)
 
 -----------------------------------------------------------------------------------------
 
